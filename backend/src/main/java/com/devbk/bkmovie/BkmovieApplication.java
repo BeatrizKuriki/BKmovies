2393c607d8ec4aab3ae317336171e38bfe268066
Beatriz Alencar Kuriki <b.f.kuriki@gmail.com>
2022-09-23 11:09:29 -0300

    feat: frontend and backend project created

---
+++ backend/src/main/java/com/devbk/bkmovie/BkmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devbk.bkmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BkmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BkmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devbk/bkmovie/BkmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbk.bkmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BkmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
